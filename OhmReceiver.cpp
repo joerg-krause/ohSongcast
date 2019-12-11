04598d2b349441ac74b61ff2a4029cc3ba8aa3a8
Jörg Krause <joerg.krause@embedded.rocks>
2019-12-11 16:27:05 +0100

    Revert "OhmReceiver: do not disconnect when reset repairing"
    This reverts commit 46beb00c23ba22c71619332e14637697bd0a3066.

--- OhmReceiver.cpp
+++ OhmReceiver.cpp
@@ -542,6 +542,11 @@ void  OhmReceiver::RepairReset()
 	while (iFifoRepair.SlotsUsed() > 0) {
 		iFifoRepair.Read()->RemoveRef();
 	}
+
+	iTransportState = eDisconnected;
+	iDriver->Disconnected();
+
+	iLatency = 0;
 }
 
 TBool OhmReceiver::Repair(OhmMsgAudio& aMsg)
